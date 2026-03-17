bf71602faad49cfc70a93f947ab1f28033235968
armbennett <armbennett@users.noreply.github.com>
2026-03-17 22:28:06 +0000

    deploy: 5c301dc4b36bb21ad36d1da4a7a6479b86028b51

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,7 +26,7 @@ runButton.addEventListener('mouseup', function (e) {
     		document.getElementById('code').value = tangible.scanCode();
     	}
     	let textCode = document.getElementById('code').value;
-    	TopCodes.stopVideoScan('video-canvas');
+    	//TopCodes.stopVideoScan('video-canvas');
     	tangible.runTextCode(textCode);
     }
 }.bind(this));
