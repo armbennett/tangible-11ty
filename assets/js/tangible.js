97028be055dacf2ea883979cbc307a08736bc8a2
armbennett <ahadwenbennett@gmail.com>
2026-09-18 01:28:17 +0100

    Update tangible.js

--- assets/js/tangible.js
+++ assets/js/tangible.js
@@ -540,11 +540,12 @@ export default class Tangible {
         let parsedLines = [];
         parsedLines.push(this.evalTile(parsedJS, this));
 
-        
+        for (let x = 0; x < 10; x++) {
         for (let i = 0; i < this.codeThreads.length; i++) {
             if (this.codeThreads[i].length > 0) {
         		this.playStart(this.threads[i],this.codeThreads[i]);
         	}
+        }
         }
         this.funcText = "";
         this.currThread = 0;
