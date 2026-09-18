550e44b489bf2680aa1dce4a02118ad9154e8cff
armbennett <armbennett@users.noreply.github.com>
2026-09-18 00:09:10 +0000

    deploy: 5b92a41c7cec40d01acbc95f43e1a707a6b8f68a

--- assets/js/tangible.js
+++ assets/js/tangible.js
@@ -62,6 +62,7 @@ export default class Tangible {
         this.attempts = 0; //keep track of number of attempts to successfully scan and run program
         this.funcText = "";
         this.funcActive = false;
+        this.repeat = true;
         this.soundSets = {
         	    OdeToJoy: { 
         	    a: [0, 1739], 
@@ -538,6 +539,8 @@ export default class Tangible {
         console.log(parsedJS);
         let parsedLines = [];
         parsedLines.push(this.evalTile(parsedJS, this));
+        while (this.repeat) {
+        
         for (let i = 0; i < this.codeThreads.length; i++) {
             if (this.codeThreads[i].length > 0) {
         		this.playStart(this.threads[i],this.codeThreads[i]);
@@ -545,6 +548,7 @@ export default class Tangible {
         }
         this.funcText = "";
         this.currThread = 0;
+        }
     }
     
     getThreadDuration() {
