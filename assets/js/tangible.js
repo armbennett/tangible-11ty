3741f0128ed9057c91f50b6df91a3d35d4978326
armbennett <ahadwenbennett@gmail.com>
2026-09-18 01:10:39 +0100

    Update tangible.js

--- assets/js/tangible.js
+++ assets/js/tangible.js
@@ -535,11 +535,12 @@ export default class Tangible {
     //runs the text code
     runTextCode(codeText) {
  		var code = codeText.toLowerCase();
+ 		 while (this.repeat) {
         let parsedJS = this.parseTextAsJavascript(code);
         console.log(parsedJS);
         let parsedLines = [];
         parsedLines.push(this.evalTile(parsedJS, this));
-        while (this.repeat) {
+
         
         for (let i = 0; i < this.codeThreads.length; i++) {
             if (this.codeThreads[i].length > 0) {
